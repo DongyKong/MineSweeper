54c6f3180f03e0e78d7260fb7363c1c593009ea0
Eric Dong <ericdong@usc.edu>
2015-01-14 19:23:05 -0800

    added default options

--- Assets/Scripts/Preferences.cs
+++ Assets/Scripts/Preferences.cs
@@ -5,10 +5,10 @@ public static class Preferences {
 	public static int[] numBoxOptions = { 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400};
 	public static int currNumBoxOption;
 
-	public static int numBoxes;
-	public static float distanceBetweenBoxes;
-	public static float fractionMines;
+	public static int numBoxes = 100;
+	public static float distanceBetweenBoxes = 0.5f;
+	public static float fractionMines = 0.25f;
 
-	public static bool startAssist;
-	public static int startAssistNumReveal;
+	public static bool startAssist = true;
+	public static int startAssistNumReveal = 10;
 }
